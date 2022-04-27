a21e577f58e3a5ce7169959b30270da559815b81
mabenj <markobenj@hotmail.com>
2022-04-28 01:45:31 +0300

    Improve device list styling

--- src/client-app/src/components/ViewDevices.tsx
+++ src/client-app/src/components/ViewDevices.tsx
@@ -10,101 +10,121 @@ import DeviceService from "../services/DeviceService";
 import { range, sortAlphabetically, timeSince } from "../utils/utils";
 
 export default function ViewDevices() {
-  const [currentDevices, setCurrentDevices] = useState<Device[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+	const [currentDevices, setCurrentDevices] = useState<Device[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchDevices() {
-      setCurrentDevices(await DeviceService.getAllDevices());
-      setIsLoading(false);
-    }
-    fetchDevices();
-  }, []);
+	useEffect(() => {
+		async function fetchDevices() {
+			setCurrentDevices(await DeviceService.getAllDevices());
+			setIsLoading(false);
+		}
+		fetchDevices();
+	}, []);
 
-  return (
-    <>
-      <div className="mb-5">
-        <h2 className="d-inline">Registered Devices</h2>
-        {isLoading && <Spinner animation="border" className="mx-3" />}
-      </div>
-      <ListGroup>
-        {isLoading &&
-          range(0, 10).map((index) => <DevicePlaceholder key={index} />)}
-        {sortAlphabetically(currentDevices, "name").map((device) => (
-          <DeviceItem key={device.id} device={device} />
-        ))}
-        {currentDevices.length < 1 && !isLoading && (
-          <Alert variant="warning">
-            No registered devices could be found — Register a new device{" "}
-            <Alert.Link as="span">
-              <Link to="/registerDevice" className="text-reset hover-underline">
-                here
-              </Link>
-            </Alert.Link>
-          </Alert>
-        )}
-      </ListGroup>
-    </>
-  );
+	return (
+		<>
+			<div className="mb-5">
+				<h2 className="d-inline">Registered Devices</h2>
+				{isLoading && <Spinner animation="border" className="mx-3" />}
+			</div>
+			<ListGroup>
+				{isLoading &&
+					range(0, 10).map((index) => <DevicePlaceholder key={index} />)}
+				{sortAlphabetically(currentDevices, "name").map((device) => (
+					<DeviceItem
+						key={device.id}
+						device={device}
+						onDeleteDevice={() =>
+							setCurrentDevices((prev) =>
+								prev.filter(({ id }) => id !== device.id)
+							)
+						}
+					/>
+				))}
+				{currentDevices.length < 1 && !isLoading && (
+					<Alert variant="warning">
+						No registered devices could be found — Register a new device{" "}
+						<Alert.Link as="span">
+							<Link to="/registerDevice" className="text-reset hover-underline">
+								here
+							</Link>
+						</Alert.Link>
+					</Alert>
+				)}
+			</ListGroup>
+		</>
+	);
 }
 
 interface DeviceCardProps {
-  device: Device;
+	device: Device;
+	onDeleteDevice: () => void;
 }
 
-const DeviceItem = ({ device }: DeviceCardProps) => {
-  return (
-    <ListGroup.Item action>
-      <Link
-        to={`/viewDevices/${device.id}`}
-        state={{ device }}
-        title={device.description}
-      >
-        <span className="d-flex justify-content-between p-2">
-          <span>
-            <span className="hover-underline">{device.name}</span>
-            <small className="text-decoration-none text-muted pe-none mx-3">
-              <em>Modified {timeSince(device.updatedAt || new Date())} ago</em>
-            </small>
-          </span>
-          <Badge pill bg={device.enabled ? "success" : "secondary"}>
-            {device.enabled ? "Enabled" : "Disabled"}
-          </Badge>
-          <Badge
-            bg="danger"
-            onClick={() => DeviceService.deleteDevice(device.id!)}
-          >
-            Delete
-          </Badge>
-        </span>
-      </Link>
-    </ListGroup.Item>
-  );
+const DeviceItem = ({ device, onDeleteDevice }: DeviceCardProps) => {
+	const deleteDevice = async (event: React.MouseEvent) => {
+		event.preventDefault();
+		onDeleteDevice();
+		DeviceService.deleteDevice(device.id!);
+	};
+
+	return (
+		<ListGroup.Item action>
+			<Link
+				to={`/viewDevices/${device.id}`}
+				state={{ device }}
+				title={device.description}>
+				<span className="d-flex justify-content-between p-2">
+					<span>
+						<span className="hover-underline">{device.name}</span>
+						<small className="text-decoration-none text-muted pe-none mx-3">
+							<em>Modified {timeSince(device.updatedAt || new Date())} ago</em>
+						</small>
+					</span>
+					<span>
+						<Badge
+							pill
+							bg={device.enabled ? "success" : "secondary"}
+							className="me-3">
+							{device.enabled ? "Enabled" : "Disabled"}
+						</Badge>
+						<Badge
+							className="hover-filter"
+							bg="danger"
+							onClick={deleteDevice}
+							title="Delete the device">
+							Delete <span className="mdi mdi-delete"></span>
+						</Badge>
+					</span>
+				</span>
+			</Link>
+		</ListGroup.Item>
+	);
 };
 
 const DevicePlaceholder = () => {
-  const maxWidthPx = 350;
-  const minWidthPx = 250;
-  return (
-    <ListGroup.Item>
-      <Placeholder animation="glow">
-        <span className="d-flex justify-content-between p-2">
-          <Placeholder
-            size="lg"
-            style={{
-              width: `${
-                Math.random() * (maxWidthPx - minWidthPx + 1) + minWidthPx
-              }px`,
-            }}
-          />
-          <Placeholder
-            size="lg"
-            style={{
-              width: "70px",
-            }}
-          />
-        </span>
-      </Placeholder>
-    </ListGroup.Item>
-  );
+	const maxWidthPx = 350;
+	const minWidthPx = 250;
+	return (
+		<ListGroup.Item>
+			<Placeholder animation="glow">
+				<span className="d-flex justify-content-between p-2">
+					<Placeholder
+						size="lg"
+						style={{
+							width: `${
+								Math.random() * (maxWidthPx - minWidthPx + 1) + minWidthPx
+							}px`
+						}}
+					/>
+					<Placeholder
+						size="lg"
+						style={{
+							width: "70px"
+						}}
+					/>
+				</span>
+			</Placeholder>
+		</ListGroup.Item>
+	);
 };
