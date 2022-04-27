d0d0d694ffa72cadfe943a4c264a2b84aaba7637
Elmeri Selänne <eljuse@utu.fi>
2022-04-28 00:26:25 +0300

    viewData view added (not implemented)

--- src/client-app/src/components/ViewData.tsx
+++ src/client-app/src/components/ViewData.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Badge } from "react-bootstrap";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { Device } from "../../../interfaces/device.interface";
 import DeviceService from "../services/DeviceService";
 
@@ -8,7 +7,7 @@ interface LocationState {
   device: Device;
 }
 
-export default function ViewDevice() {
+export default function ViewData() {
   const { deviceId } = useParams();
   const { state } = useLocation();
   const [device, setDevice] = useState<Device>();
@@ -29,15 +28,8 @@ export default function ViewDevice() {
   return (
     <div>
       <h3>Device: {device?.name}</h3>
-      <br />
-      <h1>
-        <Link to={`/viewDevices/${device?.id}/viewdata`}>
-          <Badge>View Data</Badge>
-        </Link>
-      </h1>
-      <br />
-      <pre>{JSON.stringify(device, null, 2)}</pre>
-      TODO: delete, modify, view device data, export device data (csv/json?)
+      <h3>Data:</h3>
+      TODO: show device data here
     </div>
   );
 }
--- src/client-app/src/routes/routes.ts
+++ src/client-app/src/routes/routes.ts
@@ -1,24 +1,30 @@
 import Home from "../components/Home";
 import NotFound from "../components/NotFound";
 import RegisterDevice from "../components/RegisterDevice";
+import ViewData from "../components/ViewData";
 import ViewDevice from "../components/ViewDevice";
 import ViewDevices from "../components/ViewDevices";
 
 const Routes = [
-	{ path: "/", breadcrumb: "Home", isIndex: true, Component: Home },
-	{ path: "/home", breadcrumb: "Home", Component: Home },
-	{ path: "/viewDevices", breadcrumb: "View Devices", Component: ViewDevices },
-	{
-		path: "/viewDevices/:deviceId",
-		breadcrumb: "Device Configuration",
-		Component: ViewDevice
-	},
-	{
-		path: "/registerDevice",
-		breadcrumb: "Register Device",
-		Component: RegisterDevice
-	},
-	{ path: "*", breadcrumb: "Not found", Component: NotFound }
+  { path: "/", breadcrumb: "Home", isIndex: true, Component: Home },
+  { path: "/home", breadcrumb: "Home", Component: Home },
+  { path: "/viewDevices", breadcrumb: "View Devices", Component: ViewDevices },
+  {
+    path: `/viewDevices/:deviceId/viewData`,
+    breadcrumb: "View Data",
+    Component: ViewData,
+  },
+  {
+    path: "/viewDevices/:deviceId",
+    breadcrumb: "Device Configuration",
+    Component: ViewDevice,
+  },
+  {
+    path: "/registerDevice",
+    breadcrumb: "Register Device",
+    Component: RegisterDevice,
+  },
+  { path: "*", breadcrumb: "Not found", Component: NotFound },
 ];
 
 export default Routes;
