d7cdcac33e133acf80d2e50e5c4140cb41912144
Elmeri <elmeri.selanne@gmail.com>
2022-04-27 21:47:15 +0300

    Delete functionality implemented

--- src/client-app/src/components/ViewDevices.tsx
+++ src/client-app/src/components/ViewDevices.tsx
@@ -10,94 +10,101 @@ import DeviceService from "../services/DeviceService";
 import { range, sortAlphabetically, timeSince } from "../utils/utils";
 
 export default function ViewDevices() {
-	const [currentDevices, setCurrentDevices] = useState<Device[]>([]);
-	const [isLoading, setIsLoading] = useState(true);
+  const [currentDevices, setCurrentDevices] = useState<Device[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-	useEffect(() => {
-		async function fetchDevices() {
-			setCurrentDevices(await DeviceService.getAllDevices());
-			setIsLoading(false);
-		}
-		fetchDevices();
-	}, []);
+  useEffect(() => {
+    async function fetchDevices() {
+      setCurrentDevices(await DeviceService.getAllDevices());
+      setIsLoading(false);
+    }
+    fetchDevices();
+  }, []);
 
-	return (
-		<>
-			<div className="mb-5">
-				<h2 className="d-inline">Registered Devices</h2>
-				{isLoading && <Spinner animation="border" className="mx-3" />}
-			</div>
-			<ListGroup>
-				{isLoading &&
-					range(0, 10).map((index) => <DevicePlaceholder key={index} />)}
-				{sortAlphabetically(currentDevices, "name").map((device) => (
-					<DeviceItem key={device.id} device={device} />
-				))}
-				{currentDevices.length < 1 && !isLoading && (
-					<Alert variant="warning">
-						No registered devices could be found — Register a new device{" "}
-						<Alert.Link as="span">
-							<Link to="/registerDevice" className="text-reset hover-underline">
-								here
-							</Link>
-						</Alert.Link>
-					</Alert>
-				)}
-			</ListGroup>
-		</>
-	);
+  return (
+    <>
+      <div className="mb-5">
+        <h2 className="d-inline">Registered Devices</h2>
+        {isLoading && <Spinner animation="border" className="mx-3" />}
+      </div>
+      <ListGroup>
+        {isLoading &&
+          range(0, 10).map((index) => <DevicePlaceholder key={index} />)}
+        {sortAlphabetically(currentDevices, "name").map((device) => (
+          <DeviceItem key={device.id} device={device} />
+        ))}
+        {currentDevices.length < 1 && !isLoading && (
+          <Alert variant="warning">
+            No registered devices could be found — Register a new device{" "}
+            <Alert.Link as="span">
+              <Link to="/registerDevice" className="text-reset hover-underline">
+                here
+              </Link>
+            </Alert.Link>
+          </Alert>
+        )}
+      </ListGroup>
+    </>
+  );
 }
 
 interface DeviceCardProps {
-	device: Device;
+  device: Device;
 }
 
 const DeviceItem = ({ device }: DeviceCardProps) => {
-	return (
-		<ListGroup.Item action>
-			<Link
-				to={`/viewDevices/${device.id}`}
-				state={{ device }}
-				title={device.description}>
-				<span className="d-flex justify-content-between p-2">
-					<span>
-						<span className="hover-underline">{device.name}</span>
-						<small className="text-decoration-none text-muted pe-none mx-3">
-							<em>Modified {timeSince(device.updatedAt || new Date())} ago</em>
-						</small>
-					</span>
-					<Badge pill bg={device.enabled ? "success" : "secondary"}>
-						{device.enabled ? "Enabled" : "Disabled"}
-					</Badge>
-				</span>
-			</Link>
-		</ListGroup.Item>
-	);
+  return (
+    <ListGroup.Item action>
+      <Link
+        to={`/viewDevices/${device.id}`}
+        state={{ device }}
+        title={device.description}
+      >
+        <span className="d-flex justify-content-between p-2">
+          <span>
+            <span className="hover-underline">{device.name}</span>
+            <small className="text-decoration-none text-muted pe-none mx-3">
+              <em>Modified {timeSince(device.updatedAt || new Date())} ago</em>
+            </small>
+          </span>
+          <Badge pill bg={device.enabled ? "success" : "secondary"}>
+            {device.enabled ? "Enabled" : "Disabled"}
+          </Badge>
+          <Badge
+            bg="danger"
+            onClick={() => DeviceService.deleteDevice(device.id!)}
+          >
+            Delete
+          </Badge>
+        </span>
+      </Link>
+    </ListGroup.Item>
+  );
 };
 
 const DevicePlaceholder = () => {
-	const maxWidthPx = 350;
-	const minWidthPx = 250;
-	return (
-		<ListGroup.Item>
-			<Placeholder animation="glow">
-				<span className="d-flex justify-content-between p-2">
-					<Placeholder
-						size="lg"
-						style={{
-							width: `${
-								Math.random() * (maxWidthPx - minWidthPx + 1) + minWidthPx
-							}px`
-						}}
-					/>
-					<Placeholder
-						size="lg"
-						style={{
-							width: "70px"
-						}}
-					/>
-				</span>
-			</Placeholder>
-		</ListGroup.Item>
-	);
+  const maxWidthPx = 350;
+  const minWidthPx = 250;
+  return (
+    <ListGroup.Item>
+      <Placeholder animation="glow">
+        <span className="d-flex justify-content-between p-2">
+          <Placeholder
+            size="lg"
+            style={{
+              width: `${
+                Math.random() * (maxWidthPx - minWidthPx + 1) + minWidthPx
+              }px`,
+            }}
+          />
+          <Placeholder
+            size="lg"
+            style={{
+              width: "70px",
+            }}
+          />
+        </span>
+      </Placeholder>
+    </ListGroup.Item>
+  );
 };
--- src/client-app/src/services/DeviceService.ts
+++ src/client-app/src/services/DeviceService.ts
@@ -2,44 +2,44 @@ import axios from "axios";
 import { Device } from "../../../interfaces/device.interface";
 
 async function getAllDevices(): Promise<Device[]> {
-	const response = await axios.get<Device[]>("/api/devices");
-	return response.data.map((device) => ({
-		...device,
-		createdAt: new Date(device.createdAt!),
-		updatedAt: new Date(device.updatedAt!)
-	}));
+  const response = await axios.get<Device[]>("/api/devices");
+  return response.data.map((device) => ({
+    ...device,
+    createdAt: new Date(device.createdAt!),
+    updatedAt: new Date(device.updatedAt!),
+  }));
 }
 
 async function getDevice(deviceId: string): Promise<Device> {
-	const { data } = await axios.get<Device>(`/api/devices/${deviceId}`);
-	return {
-		...data,
-		createdAt: new Date(data.createdAt!),
-		updatedAt: new Date(data.updatedAt!)
-	};
+  const { data } = await axios.get<Device>(`/api/devices/${deviceId}`);
+  return {
+    ...data,
+    createdAt: new Date(data.createdAt!),
+    updatedAt: new Date(data.updatedAt!),
+  };
 }
 
 async function registerDevice(device: Device) {
-	const response = await axios.post("/api/devices", device);
-	return response.data;
+  const response = await axios.post("/api/devices", device);
+  return response.data;
 }
 
 async function deleteDevice(deviceId: string) {
-	//TODO
-	throw new Error("Not implemented");
+  const response = await axios.delete(`api/devices/${deviceId}`);
+  return response.data;
 }
 
 async function modifyDevice(device: Device) {
-	//TODO
-	throw new Error("Not implemented");
+  //TODO
+  console.log("Modify button pressed!");
 }
 
 const DeviceService = {
-	getAllDevices,
-	registerDevice,
-	deleteDevice,
-	modifyDevice,
-	getDevice
+  getAllDevices,
+  registerDevice,
+  deleteDevice,
+  modifyDevice,
+  getDevice,
 };
 
 export default DeviceService;
