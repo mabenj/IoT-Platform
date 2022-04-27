dc1ada1603d3e0d4f5b81a95199e52eb555fcb74
mabenj <markobenj@hotmail.com>
2022-04-28 02:10:54 +0300

    Implement getAllDeviceData

--- src/client-app/src/services/DeviceDataService.ts
+++ src/client-app/src/services/DeviceDataService.ts
@@ -1,11 +1,17 @@
+import axios from "axios";
+import { DeviceData } from "../../../interfaces/device-data.interface";
+
 async function getAllDeviceData(deviceId: string) {
-	//TODO
-	throw new Error("Not implemented");
+    const response = await axios.get<DeviceData[]>("/api/deviceData");
+    return response.data.map((deviceDatum) => ({
+        ...deviceDatum,
+        createdAt: new Date(deviceDatum.createdAt)
+    }));
 }
 
 async function deleteAllDeviceData(deviceId: string) {
-	//TODO
-	throw new Error("Not implemented");
+    //TODO
+    throw new Error("Not implemented");
 }
 
 const DeviceDataService = { getAllDeviceData, deleteAllDeviceData };
