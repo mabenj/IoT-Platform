f88f0e81b3f6189f5a6c11160e56524ae05d73de
mabenj <markobenj@hotmail.com>
2022-04-30 01:19:59 +0300

    Update coap and http api port fallbacks

--- src/api/configs/coap-api.config.ts
+++ src/api/configs/coap-api.config.ts
@@ -1,13 +1,13 @@
 import "dotenv/config";
 
 interface CoapApiConfig {
-	incomingRequestDelayMs: number;
-	port: string | number;
+    incomingRequestDelayMs: number;
+    port: string | number;
 }
 
 const config: CoapApiConfig = {
-	incomingRequestDelayMs: 1500,
-	port: process.env.COAP_PORT || 7200
+    incomingRequestDelayMs: 1500,
+    port: process.env.COAP_PORT || process.env.PORT || 7200
 };
 
 export default config;
--- src/api/configs/http-api.config.ts
+++ src/api/configs/http-api.config.ts
@@ -1,13 +1,13 @@
 import "dotenv/config";
 
 interface HttpApiConfig {
-	incomingRequestDelayMs: number;
-	port: string | number;
+    incomingRequestDelayMs: number;
+    port: string | number;
 }
 
 const config: HttpApiConfig = {
-	incomingRequestDelayMs: 1500,
-	port: process.env.HTTP_PORT || 7100
+    incomingRequestDelayMs: 1500,
+    port: process.env.HTTP_PORT || process.env.PORT || 7100
 };
 
 export default config;
