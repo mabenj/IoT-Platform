eaee7648cc7a6b195f9467a16b938ae72cfcc77a
mabenj <markobenj@hotmail.com>
2022-12-15 00:35:38 +0200

    Adjust postBuild

--- postBuild.js
+++ postBuild.js
@@ -1,5 +1,9 @@
 const apiRedirect = process.argv[2];
 if (apiRedirect) {
     const fs = require("fs");
-    fs.writeFile("_redirects", `/api/* ${apiRedirect}/:splat`, () => null);
+    fs.writeFile(
+        "dist/client-app/_redirects",
+        `/api/* ${apiRedirect}/:splat`,
+        () => null
+    );
 }
