5f3432ad1e6820bc431c51e1a9d9f0d5e6dc0a5f
mabenj <markobenj@hotmail.com>
2022-12-15 18:00:55 +0200

    Modify redirects

--- postBuild.js
+++ postBuild.js
@@ -1,9 +1,13 @@
 const apiRedirect = process.argv[2];
 if (apiRedirect) {
+    const redirects = [
+        `/api/*   ${apiRedirect}/:splat 200`,
+        `/*       /index.html           200`
+    ];
     const fs = require("fs");
     fs.writeFile(
         "dist/client-app/_redirects",
-        `/api/* ${apiRedirect}/:splat 200`,
+        redirects.join("\n"),
         () => null
     );
 }
